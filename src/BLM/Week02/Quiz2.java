19d6ddacdc94c452784c87cc7890c9d7b959f2f8
alinizam <alinizam99@gmail.com>
2025-12-31 14:29:16 +0300

    Week 14 Small chance

--- src/BLM/Week02/Quiz2.java
+++ src/BLM/Week02/Quiz2.java
@@ -2,7 +2,7 @@ package BLM.Week02;
 
 import java.util.ArrayList;
 
-public class main {
+public class Quiz2 {
 
     public static void main(String[] args) {
         Employee [] employees = new Employee[7];
